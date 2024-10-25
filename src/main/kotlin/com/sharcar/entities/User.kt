af53f0e56ffbe5fadbfaac1cb4a08eba5cb28cac
JaviGP2002 <javigp2002@gmail.com>
2024-10-25 17:37:39 +0200

    :bug: Delete attribute: phone number to email, #3

--- src/main/kotlin/com/sharcar/entities/User.kt
+++ src/main/kotlin/com/sharcar/entities/User.kt
@@ -2,10 +2,10 @@ package com.sharcar.entities
 
 data class User(
     val id: Int,
+    val email: String,
     val name: String,
     val surname: String,
     val password: String,
-    val phoneNumber: String,
     val vehicles: List<Vehicle>,
     val enterprise: Enterprise
 )
