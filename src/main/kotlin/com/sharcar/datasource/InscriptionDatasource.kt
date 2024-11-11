e9537d1993b2ad2d8113b8fbdac6afefccde95e5
JaviGP2002 <javigp2002@gmail.com>
2024-11-11 19:38:44 +0100

    :rocket: Add: Implement fun related to passenger travels, #14

--- src/main/kotlin/com/sharcar/datasource/InscriptionDatasource.kt
+++ src/main/kotlin/com/sharcar/datasource/InscriptionDatasource.kt
@@ -2,6 +2,7 @@ package com.sharcar.datasource
 
 import com.sharcar.domain.InscriptionRepository
 import com.sharcar.entities.Inscription
+import com.sharcar.entities.User
 
 class InscriptionDatasource: InscriptionRepository {
     private val inscriptions = mutableListOf<Inscription>()
@@ -9,6 +10,32 @@ class InscriptionDatasource: InscriptionRepository {
     override fun save(inscription: Inscription): Inscription {
         inscriptions.add(inscription)
         return inscription
+    }
+
+    override fun updatePassengerIntoInscription(inscriptionId: Int, passenger: User): Boolean {
+        val inscription = getInscriptionsById(inscriptionId) ?: return false
+        if (inscription.vehicle.maxPassengers <= inscription.passengers.size)
+            return false
+
+        return updatePassengerIntoInscription(inscription, passenger)
+    }
+
+    override fun getInscriptionsOfEnterprises(enterpriseId: Int): Inscription? {
+        return inscriptions.find { it.enterprise.id == enterpriseId }
+    }
+
+    override fun getSeatsAvailable(inscriptionId: Int): Int {
+        val inscription = getInscriptionsById(inscriptionId) ?: return 0
+        return inscription.vehicle.maxPassengers - inscription.passengers.size
+    }
+
+    private fun getInscriptionsById(id: Int): Inscription? {
+        return inscriptions.find{ it.id == id }
+    }
+
+    private fun updatePassengerIntoInscription(inscription: Inscription, passenger: User): Boolean {
+        inscription.passengers.add(passenger)
+        return true
     }
 
 }
--- src/main/kotlin/com/sharcar/domain/InscriptionRepository.kt
+++ src/main/kotlin/com/sharcar/domain/InscriptionRepository.kt
@@ -1,6 +1,10 @@
 package com.sharcar.domain
 import com.sharcar.entities.Inscription
+import com.sharcar.entities.User
 
 interface InscriptionRepository {
     fun save(inscription: Inscription): Inscription
+    fun updatePassengerIntoInscription(inscriptionId: Int, passenger: User): Boolean
+    fun getInscriptionsOfEnterprises(enterpriseId: Int): Inscription?
+    fun getSeatsAvailable(inscriptionId: Int): Int
 }
