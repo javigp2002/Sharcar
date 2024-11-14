2f3eb4ae264a00ca75c58557af84ce0fb2010dfa
JaviGP2002 <javigp2002@gmail.com>
2024-11-14 12:06:22 +0100

    :white_check_mark: Tests for all the alternatives on creating travel

--- src/test/kotlin/com/sharcar/usecases/travel/CreateTravelTest.kt
+++ src/test/kotlin/com/sharcar/usecases/travel/CreateTravelTest.kt
@@ -5,16 +5,19 @@ import com.sharcar.domain.usecases.model.InscriptionModel
 import com.sharcar.domain.usecases.travel.CreateTravelUsecase
 import com.sharcar.entities.*
 import org.junit.Before
-import org.mockito.*
+import org.mockito.Mock
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.`when`
 import java.util.*
 import kotlin.test.Test
-import kotlin.test.*
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 
 class CreateTravelTest {
+    private val currentTime = Date()
     private lateinit var vehicle1: Vehicle
+    private lateinit var vehicle2: Vehicle
     private lateinit var location1: Locations
     private lateinit var enterprise1: Enterprise
     private lateinit var driver: User
@@ -27,8 +30,8 @@ class CreateTravelTest {
 
     @Before
     fun init() {
-        val currentTime = Date()
         vehicle1 = Vehicle(1, "Clio", "Renault", "Clio", 2016, 4)
+        vehicle2 = Vehicle(2, "Clio", "Renault", "Clio", 2015, 4)
         location1 = Locations(1, "Granada", "Recogidas 14", "La caleta")
         enterprise1 = Enterprise(1, "Enterprise1", mutableListOf(location1))
         driver = User("javi@gmail.com", "driver", "Sarmiento", "pass", mutableListOf(vehicle1), enterprise1)
@@ -56,5 +59,103 @@ class CreateTravelTest {
         assertTrue(!creationInscriptionResult.success)
     }
 
+    @Test
+    fun `Return alternative FALSE if not seats available on similar inscription`() {
+        val newUser = User("antonio@gmail.com", "driver", "Sarmiento", "pass", mutableListOf(vehicle2), enterprise1)
+
+        val fiveMins = 5 * 60 * 1000
+        val inscriptionAlreadyExists =
+            Inscription(
+                2,
+                enterprise1,
+                Date(currentTime.time + fiveMins),
+                "Gasolinera Neptuno",
+                location1,
+                newUser,
+                mutableListOf(
+                    driver, driver, driver, driver
+                ),
+                vehicle2
+            )
+
+        `when`(inscriptionRepository.getInscriptionsOfEnterprises(enterprise1.id)).thenReturn(
+            mutableListOf(inscriptionAlreadyExists)
+        )
+        `when`(inscriptionRepository.save(model)).thenReturn(
+            inscription1
+        )
+
+        val creationInscriptionResult = createTravel.run(model)
+        assertTrue(creationInscriptionResult.success && !creationInscriptionResult.alternative)
+    }
+
+    @Test
+    fun `Return alternative FALSE if similar inscription`() {
+        `when`(inscriptionRepository.getInscriptionsOfEnterprises(enterprise1.id)).thenReturn(
+            mutableListOf()
+        )
+        `when`(inscriptionRepository.save(model)).thenReturn(
+            inscription1
+        )
+
+        val creationInscriptionResult = createTravel.run(model)
+        assertTrue(creationInscriptionResult.success && !creationInscriptionResult.alternative)
+    }
+
+    @Test
+    fun `Return alternative TRUE if similar inscription and able to seat`() {
+        val newUser = User("antonio@gmail.com", "driver", "Sarmiento", "pass", mutableListOf(vehicle2), enterprise1)
+
+        val fiveMins = 5 * 60 * 1000
+        val inscriptionAlreadyExists =
+            Inscription(
+                2,
+                enterprise1,
+                Date(currentTime.time + fiveMins),
+                "Gasolinera Neptuno",
+                location1,
+                newUser,
+                mutableListOf(),
+                vehicle2
+            )
+
+        `when`(inscriptionRepository.getInscriptionsOfEnterprises(enterprise1.id)).thenReturn(
+            mutableListOf(inscriptionAlreadyExists)
+        )
+        `when`(inscriptionRepository.save(model)).thenReturn(
+            inscription1
+        )
+
+        val creationInscriptionResult = createTravel.run(model)
+        assertTrue(creationInscriptionResult.success && creationInscriptionResult.alternative)
+    }
+
+    @Test
+    fun `Return new inscription created`() {
+        val newUser = User("antonio@gmail.com", "driver", "Sarmiento", "pass", mutableListOf(vehicle2), enterprise1)
+
+        val fiveMins = 5 * 60 * 1000
+        val inscriptionAlreadyExists =
+            Inscription(
+                2,
+                enterprise1,
+                Date(currentTime.time + fiveMins),
+                "Gasolinera Neptuno",
+                location1,
+                newUser,
+                mutableListOf(),
+                vehicle2
+            )
+
+        `when`(inscriptionRepository.getInscriptionsOfEnterprises(enterprise1.id)).thenReturn(
+            mutableListOf(inscriptionAlreadyExists)
+        )
+        `when`(inscriptionRepository.save(model)).thenReturn(
+            inscription1
+        )
+
+        val creationInscriptionResult = createTravel.run(model)
+        assertEquals(inscription1.id, creationInscriptionResult.inscription?.id)
+    }
 
 }
