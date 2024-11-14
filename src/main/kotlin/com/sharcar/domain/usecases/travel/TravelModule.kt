28b7008d887e192e29760b3a01eb2ce8bf793e2f
JaviGP2002 <javigp2002@gmail.com>
2024-11-14 14:12:45 +0100

    :rocket: Add: DI to swap travel , #15

--- src/main/kotlin/com/sharcar/domain/usecases/travel/TravelModule.kt
+++ src/main/kotlin/com/sharcar/domain/usecases/travel/TravelModule.kt
@@ -1,8 +1,10 @@
 import com.sharcar.domain.usecases.travel.AddPassengerToTravel
 import com.sharcar.domain.usecases.travel.CreateTravelUsecase
+import com.sharcar.domain.usecases.travel.SwapTravelToNewOne
 import org.koin.dsl.module
 
 val travelModule = module {
     single { CreateTravelUsecase(get()) }
     single { AddPassengerToTravel(get(), get()) }
+    single { SwapTravelToNewOne(get(), get()) }
 }
