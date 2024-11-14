aa92477228423af005c4049d1e81777dd29bff5b
JaviGP2002 <javigp2002@gmail.com>
2024-11-14 12:38:18 +0100

    :boom: Add: Passenger to inscription if is able to, #14

--- src/main/kotlin/com/sharcar/domain/usecases/travel/TravelModule.kt
+++ src/main/kotlin/com/sharcar/domain/usecases/travel/TravelModule.kt
@@ -1,6 +1,8 @@
+import com.sharcar.domain.usecases.travel.AddPassengerToTravel
 import com.sharcar.domain.usecases.travel.CreateTravelUsecase
 import org.koin.dsl.module
 
 val travelModule = module {
     single { CreateTravelUsecase(get()) }
+    single { AddPassengerToTravel(get(), get()) }
 }
