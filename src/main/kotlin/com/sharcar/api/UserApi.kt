bfe8d5b89477375a4ea2772a9d987bb94d19e405
JaviGP2002 <javigp2002@gmail.com>
2024-11-27 00:29:55 +0100

    :bug: Add try catch if receive fails, #19

--- src/main/kotlin/com/sharcar/api/UserApi.kt
+++ src/main/kotlin/com/sharcar/api/UserApi.kt
@@ -2,6 +2,7 @@ package com.sharcar.api
 
 import com.sharcar.api.dto.CreateUserDto
 import com.sharcar.domain.usecases.user.CreateUserUsecase
+import com.sharcar.exception.SharCarBadRequestException
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -9,12 +10,17 @@ import io.ktor.server.routing.*
 fun Route.userRoutes(createUserUsecase: CreateUserUsecase) {
     route("/user") {
         post("/register") {
-            val user = call.receive<CreateUserDto>()
-            call.application.environment.log.info("Creating user: ${user.email}")
+            try {
+                val user = call.receive<CreateUserDto>()
+                call.application.environment.log.info("Creating user: ${user.email}")
 
-            createUserUsecase.run(user.email, user.name, user.surname, user.password)
+                createUserUsecase.run(user.email, user.name, user.surname, user.password)
 
-            call.respond(mapOf("email" to user.email))
+                call.respond(mapOf("email" to user.email))
+            } catch (e: Exception) {
+                call.application.environment.log.error("Error creating user", e)
+                throw SharCarBadRequestException.create(601)
+            }
         }
     }
 }
