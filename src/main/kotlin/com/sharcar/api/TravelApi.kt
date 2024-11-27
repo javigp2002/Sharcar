0e52e76ba8084cd5860544a4b2d370bd978c762e
JaviGP2002 <javigp2002@gmail.com>
2024-11-28 00:20:29 +0100

    :rocket: Add creation of route from api, #17

--- src/main/kotlin/com/sharcar/api/TravelApi.kt
+++ src/main/kotlin/com/sharcar/api/TravelApi.kt
@@ -1,10 +1,24 @@
 package com.sharcar.api
 
+import com.sharcar.api.dto.CreateTravelDto
+import com.sharcar.domain.usecases.model.CreationInscriptionResult
+import com.sharcar.domain.usecases.travel.CreateTravelUsecase
+import io.ktor.server.request.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
-fun Route.travelRoutes() {
+fun Route.travelRoutes(createTravelUsecase: CreateTravelUsecase) {
     route("/travel") {
         post("/create") {
+
+            val travel = call.receive<CreateTravelDto>()
+
+            val createTravelResult: CreationInscriptionResult = createTravelUsecase.run(travel.toCreateTravelModel())
+
+            call.respond(createTravelResult)
+
+
+
         }
 
         post("/addPassenger") {
