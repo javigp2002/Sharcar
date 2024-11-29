65950c04f9f26f3151715669ae0367af4b7ead2c
JaviGP2002 <javigp2002@gmail.com>
2024-11-29 12:41:58 +0100

    :white_check_mark: Test create travel works as expected, #29

---
+++ src/test/kotlin/com/api/travelroutes/TravelRoutesTest.kt
@@ -0,0 +1,123 @@
+package com.api.travelroutes
+
+import com.sharcar.api.dto.CreateTravelDto
+import com.sharcar.api.travelRoutes
+import com.sharcar.domain.usecases.model.CreationInscriptionResult
+import com.sharcar.domain.usecases.travel.AddPassengerToTravel
+import com.sharcar.domain.usecases.travel.CreateTravelUsecase
+import com.sharcar.domain.usecases.travel.SwapTravelToNewOne
+import io.ktor.client.call.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.routing.*
+import io.ktor.server.testing.*
+import org.mockito.Mock
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.`when`
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+
+class TravelRoutesTest {
+    @Mock
+    private val mockCreateTravelUsecase = mock<CreateTravelUsecase>()
+    private val mockAddPassengerToTravel = mock<AddPassengerToTravel>()
+    private val mockSwapTravelToNewOne = mock<SwapTravelToNewOne>()
+
+    fun TestApplicationBuilder.configureTestApplication() {
+        application {
+            install(io.ktor.server.plugins.contentnegotiation.ContentNegotiation) {
+                json()
+            }
+            routing {
+                travelRoutes(
+                    createTravelUsecase = mockCreateTravelUsecase,
+                    addPassengerUsecase = mockAddPassengerToTravel,
+                    swapTravelToNewOneUsecase = mockSwapTravelToNewOne
+                )
+            }
+        }
+    }
+
+    @Test
+    fun `CREATE TRAVEL-Creates travel and RETURNS with alternative`() = testApplication {
+        configureTestApplication()
+        val client = createClient {
+            install(ContentNegotiation) {
+                json()
+            }
+        }
+
+        val dto = CreateTravelDto(
+            1, "2007-12-03T10:15:30", "Gasolinera", 1,
+            "usuario@gmail.com", 1
+        )
+
+        `when`(mockCreateTravelUsecase.run(dto.toCreateTravelModel())).thenReturn(
+            CreationInscriptionResult(1, true, true)
+        )
+
+        val response = client.post("/travel/create") {
+            contentType(ContentType.Application.Json)
+            setBody(dto)
+        }
+        assertEquals(HttpStatusCode.OK, response.status)
+        val result = response.body<CreationInscriptionResult>()
+        assertEquals(CreationInscriptionResult(1, true, true), result)
+    }
+
+    @Test
+    fun `CREATE TRAVEL-Everything ok but Travel Already created`() = testApplication {
+        configureTestApplication()
+        val client = createClient {
+            install(ContentNegotiation) {
+                json()
+            }
+        }
+
+        val dto = CreateTravelDto(
+            1, "2007-12-03T10:15:30", "Gasolinera", 1,
+            "usuario@gmail.com", 1
+        )
+
+        `when`(mockCreateTravelUsecase.run(dto.toCreateTravelModel())).thenReturn(
+            CreationInscriptionResult(0, false, false)
+        )
+
+        val response = client.post("/travel/create") {
+            contentType(ContentType.Application.Json)
+            setBody(dto)
+        }
+        assertEquals(HttpStatusCode.OK, response.status)
+        val result = response.body<CreationInscriptionResult>()
+        assertEquals(CreationInscriptionResult(0, false, false), result)
+    }
+
+    @Test
+    fun `CREATE TRAVEL-DTO didnt match with operation`() = testApplication {
+        configureTestApplication()
+        val client = createClient {
+            install(ContentNegotiation) {
+                json()
+            }
+        }
+
+        val response = client.post("/travel/create") {
+            contentType(ContentType.Application.Json)
+            setBody(
+                """
+                {
+                    "id": 1,
+                    "date": "2007-12-03T10:15:30",
+                    "place": "Gasolinera",
+                    "driverId": 1,
+                }
+            """.trimIndent()
+            )
+        }
+        assertEquals(HttpStatusCode.BadRequest, response.status)
+    }
+}
