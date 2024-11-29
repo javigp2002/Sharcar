5209b145f0b771574fa65e3cee917e854dbf8509
JaviGP2002 <javigp2002@gmail.com>
2024-11-29 13:32:19 +0100

    :rocket: Add creation of enterprise from api, closes #11

--- src/main/kotlin/com/sharcar/api/EnterpriseApi.kt
+++ src/main/kotlin/com/sharcar/api/EnterpriseApi.kt
@@ -1,6 +1,10 @@
 package com.sharcar.api
 
+import com.sharcar.api.dto.RegisterEnterpriseDto
 import com.sharcar.domain.usecases.enterprise.CreateEnterprise
+import com.sharcar.domain.usecases.model.EnterpriseResult
+import io.ktor.server.request.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Route.enterpriseRoutes(
@@ -8,7 +12,11 @@ fun Route.enterpriseRoutes(
 ) {
     route("/enterprise") {
         post("/register") {
+            val enterprise = call.receive<RegisterEnterpriseDto>()
 
+            val createEnterpriseResult: EnterpriseResult = createEnterpriseUsecase.run(enterprise.toEnterpriseModel())
+
+            call.respond(createEnterpriseResult)
         }
     }
 }
