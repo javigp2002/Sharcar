b023ea7532e7d6c06fd0d273ac534e6883cb4c6e
JaviGP2002 <javigp2002@gmail.com>
2024-11-29 13:18:11 +0100

    :rocket: Better: better to handle a model than different variables, #11

--- src/main/kotlin/com/sharcar/domain/usecases/enterprise/CreateEnterprise.kt
+++ src/main/kotlin/com/sharcar/domain/usecases/enterprise/CreateEnterprise.kt
@@ -2,13 +2,14 @@ package com.sharcar.domain.usecases.enterprise
 
 import com.sharcar.domain.repository.enterprise.EnterpriseRepository
 import com.sharcar.entities.Enterprise
+import com.sharcar.models.CreateEnterpriseModel
 
 class CreateEnterprise (private val enterpriseRepository: EnterpriseRepository){
-    fun run(nif: Int, name: String): Enterprise{
-        val isEnterpriseCreated = enterpriseRepository.findById(nif)
-        require(isEnterpriseCreated != null) { "Enterprise with nif $nif already exists"}
+    fun run(enterpriseModel: CreateEnterpriseModel): Enterprise {
+        val isEnterpriseCreated = enterpriseRepository.findById(enterpriseModel.nif)
+        require(isEnterpriseCreated != null) { "Enterprise with nif ${enterpriseModel.nif} already exists" }
 
-        val enterprise = Enterprise(nif, name, emptyList())
+        val enterprise = Enterprise(enterpriseModel.nif, enterpriseModel.name, emptyList())
         val wasCreated =  enterpriseRepository.save(enterprise)
         require(wasCreated) { "Enterprise could not be created" }
         return enterprise
--- src/test/kotlin/com/sharcar/usecases/enterprise/CreateEnterpriseTest.kt
+++ src/test/kotlin/com/sharcar/usecases/enterprise/CreateEnterpriseTest.kt
@@ -1,14 +1,15 @@
 package com.sharcartests.domain
 
 import com.sharcar.domain.repository.enterprise.EnterpriseRepositoryImpl
-import com.sharcar.entities.Enterprise
 import com.sharcar.domain.usecases.enterprise.CreateEnterprise
+import com.sharcar.entities.Enterprise
+import com.sharcar.models.CreateEnterpriseModel
 import org.junit.jupiter.api.assertThrows
-import org.mockito.*
+import org.mockito.Mock
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.`when`
 import kotlin.test.Test
-import kotlin.test.*
+import kotlin.test.assertEquals
 
 
 class CreateEnterpriseTest{
@@ -19,20 +20,23 @@ class CreateEnterpriseTest{
      @Test
      fun `Checks that enterprise is already created`() {
          `when`(enterpriseDatasourceMock.findById(123456789)).thenReturn(null)
+         val model = CreateEnterpriseModel(123456789, "name")
 
          val exception = assertThrows<IllegalArgumentException> {
-             createEnterprise.run(123456789, "name")
+             createEnterprise.run(model)
          }
          assertEquals("Enterprise with nif 123456789 already exists", exception.message)
      }
 
      @Test
      fun `Checks that ENTERPRISE is CREATED`() {
-         `when`(enterpriseDatasourceMock.findById(987654321)).thenAnswer { Enterprise(987654321, "name", emptyList()) }
-         `when`(enterpriseDatasourceMock.save(Enterprise(987654321, "name", emptyList()))).thenReturn(true)
+         val nif = 987654321
+         `when`(enterpriseDatasourceMock.findById(nif)).thenAnswer { Enterprise(nif, "name", emptyList()) }
+         `when`(enterpriseDatasourceMock.save(Enterprise(nif, "name", emptyList()))).thenReturn(true)
 
-         val newNif = 987654321
-         val enterprise: Enterprise = createEnterprise.run(newNif, "name")
-         assertEquals(enterprise.id, newNif)
+         val model = CreateEnterpriseModel(nif, "name")
+
+         val enterprise: Enterprise = createEnterprise.run(model)
+         assertEquals(enterprise.id, nif)
      }
 }
