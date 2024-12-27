7b7b2bda8767b6c10ddbead6224e15ed5680685b
JaviGP2002 <javigp2002@gmail.com>
2024-12-27 12:40:03 +0100

    :rocket: Implement enterprise datasource connected with database, closes #24

--- src/main/kotlin/com/sharcar/exception/StatusExceptionCode.kt
+++ src/main/kotlin/com/sharcar/exception/StatusExceptionCode.kt
@@ -4,4 +4,5 @@ package com.sharcar.exception
 val mapForBadRequests = mapOf(
     601 to "User Parameters didnt match the requirements",
     602 to "User already exists",
+    701 to "Enterprise Parameters didnt match the requirements",
 )
