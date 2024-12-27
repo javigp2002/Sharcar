bd3ffbc187f6bfcb1a0bcd3be31173330bb12885
JaviGP2002 <javigp2002@gmail.com>
2024-12-27 18:33:36 +0100

    :rocket: Implement Location Datasource to take it from database, closes #22

--- src/main/kotlin/com/sharcar/datasource/location/LocationDatasource.kt
+++ src/main/kotlin/com/sharcar/datasource/location/LocationDatasource.kt
@@ -1,6 +1,28 @@
 package com.sharcar.datasource.location
 
-import com.sharcar.domain.repository.location.LocationRepository
+import com.sharcar.datasource.DatabaseConnection
+import com.sharcar.entities.Locations
 
-class LocationDatasource: LocationRepository {
+class LocationDatasource {
+    private val datasource = DatabaseConnection
+
+    fun findById(locationId: Int): Locations? {
+        val result = datasource.executeQuery(
+            """
+            SELECT * FROM Locations
+            WHERE id = ?
+        """,
+            listOf<Any>(locationId)
+        )
+        if (!result.next())
+            return null
+
+        return Locations(
+            id = result.getInt("id"),
+            city = result.getString("city"),
+            street = result.getString("street"),
+            name = result.getString("name"),
+        )
+    }
+
 }
